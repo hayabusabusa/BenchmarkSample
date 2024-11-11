7aeef8a9834848227aa159a7ab1df2c6f1ddf6e6
hayabusabusa <hayabusabusaSY@gmail.com>
2024-11-12 01:09:26 +0900

    chore: Firebase 関連のパッケージを少し追加

--- Package/Sources/ClientLive/Live.swift
+++ Package/Sources/ClientLive/Live.swift
@@ -7,8 +7,16 @@
 
 import Client
 import Dependencies
+import FirebaseAnalytics
 import FirebaseAuth
+import FirebaseCrashlytics
 import FirebaseFirestore
+import FirebaseFunctions
+import FirebaseMessaging
+import FirebasePerformance
+import FirebaseRemoteConfig
+import FirebaseStorage
+import FirebaseVertexAI
 import Foundation
 
 extension Client: DependencyKey {
@@ -18,7 +26,15 @@ extension Client: DependencyKey {
 
     private static func live() -> Self {
         let _ = Auth.auth()
+        let _ = Analytics.appInstanceID()
+        let _ = Crashlytics.crashlytics()
         let _ = Firestore.firestore()
+        let _ = Functions.functions()
+        let _ = Messaging.messaging()
+        let _ = Performance()
+        let _ = RemoteConfig.remoteConfig()
+        let _ = Storage.storage()
+        let _ = VertexAI.vertexAI()
         return .init()
     }
 }
